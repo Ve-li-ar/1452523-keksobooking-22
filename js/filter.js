6034e04f26bb35d73c8e1e692edc91fd7bf078e1
helly veliar <velar.helly@gmail.com>
2021-03-22 15:25:08 +0300

    Поправки 8.1. Надеюсь окончательные

--- js/filter.js
+++ js/filter.js
@@ -3,25 +3,16 @@ import { initMainPins, removeMarkers } from './map.js';
 const houseType = document.querySelector('#housing-type');
 const mapFilter = document.querySelector('.map__filters');
 
-
 const setHousingTypeChange = (pins) => {
-  let filterPins = [];
-  mapFilter.addEventListener('change', function () {
-
-    if (housingFilter(pin.offer)) {
-      removeMarkers();
-
-      filterPins = [];
-      pins.forEach((pin) => {
-        if (housingFilter(pin.offer)) {
-          filterPins.push(pin);
-        }
-      })
-      initMainPins(filterPins);
-    }
-    else {
-      initMainPins(pins);
-    }
+  mapFilter.addEventListener('change', () => {
+    removeMarkers();
+    const filterPins = [];
+    pins.forEach((pin) => {
+      if (housingFilter(pin.offer)) {
+        filterPins.push(pin);
+      }
+    })
+    initMainPins(filterPins);
   })
 }
 
