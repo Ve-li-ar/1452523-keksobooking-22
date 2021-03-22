1aa9d4ab9f52901abbb07127c0df45d79ae7a7ab
helly veliar <velar.helly@gmail.com>
2021-03-22 18:24:42 +0300

    8.1

--- js/filter.js
+++ js/filter.js
@@ -1,14 +1,24 @@
 import { initMainPins, removeMarkers } from './map.js';
 
-const houseType = document.querySelector('#housing-type');
 const mapFilter = document.querySelector('.map__filters');
+const houseType = document.querySelector('#housing-type');
+const houseRooms = document.querySelector('#housing-rooms');
+const houseGuests = document.querySelector('#housing-guests');
+const housePrice = document.querySelector('#housing-price');
+
+
 
 const setHousingTypeChange = (pins) => {
   mapFilter.addEventListener('change', () => {
     removeMarkers();
     const filterPins = [];
     pins.forEach((pin) => {
-      if (housingFilter(pin.offer)) {
+      if (
+        housingTypeFilter(pin.offer) &&
+        housingPriceFilter(pin.offer) &&
+        housingRoomsFilter(pin.offer) &&
+        housingCapacityFilter(pin.offer)
+      ) {
         filterPins.push(pin);
       }
     })
@@ -16,9 +26,25 @@ const setHousingTypeChange = (pins) => {
   })
 }
 
-const housingFilter = (offer) => {
+//тип
+const housingTypeFilter = (offer) => {
   return houseType.value === 'any' || offer.type === houseType.value;
-}
+};
+
+//комнаты
+const housingRoomsFilter = (offer) => {
+  return houseRooms.value === 'any' || offer.rooms === houseRooms.value;
+};
+
+//количество гостей
+const housingCapacityFilter = (offer) => {
+  return houseGuests.value === 'any' || offer.guests === houseGuests.value;
+};
+
+//цена
+const housingPriceFilter = (offer) => {
+  return housePrice.value === 'any' || offer.price === housePrice.value;
+};
 
 const resetFilterForm = () => {
   mapFilter.reset();
